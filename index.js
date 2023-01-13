5fe1f381d3729d92e81ff5f9e9eb32f1d4e15e07
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-01-13 20:17:36 +0100

    task2

--- index.js
+++ index.js
@@ -9,21 +9,40 @@
 //premium: false
 //mood: happy
 
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tenis",
-  premium: true,
-};
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tenis",
+//   premium: true,
+// };
 
-user.mood = "happy";
-console.log(user);
-user.hobby = 'skydiving';
-console.log(user);
-user.premium = false;
-console.log(user);
+// user.mood = "happy";
+// console.log(user);
+// user.hobby = 'skydiving';
+// console.log(user);
+// user.premium = false;
+// console.log(user);
 
+// for (const key of Object.keys(user)) {
+//     console.log(`${key}: ${user[key]}`)
+// }
 
-for (const key of Object.keys(user)) {
-    console.log(`${key}: ${user[key]}`)
-}
+//2. Є об'єкт, в якому зберігаються зарплати команди. Напишіть функцію getTotalSalary, яка буде повертати загальну суму зарплат. Якщо об'єкт пустий, то функція має повернути 0
+
+// const salaries = {
+//   mango: 100,
+//   poly: 160,
+//   ajax: 1470,
+// };
+
+// function getTotalSalary() {
+//   const allSalaries = Object.values(salaries);
+//   let totalSalary = 0;
+//   for (let i = 0; i < allSalaries.length; i += 1) {
+//     totalSalary += allSalaries[i];
+//   }
+
+//   return totalSalary;
+// }
+
+// console.log(getTotalSalary());
