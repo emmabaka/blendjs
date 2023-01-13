7cfba1db5d51aeddb83470325b729f7ae6447ad6
Oleksandr <alex.vovchok@gmail.com>
2023-01-13 22:22:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -72,3 +72,45 @@
 //    }, 0)
 // };
 // console.log(calcTotalPrice(stones, "sapphire"));
+
+//4. Створіть об'єкт calculator з трьома методами
+//read(a, b) - приймає два аргумента та зберігає їх як властивості об'єкта під ключами prop1 та prop2
+//sum() перевіряє чи присутні  властивості prop1/prop2 та повертає їх суму, в противному випадку повертає null
+//mult() перевіряє чи присутні  властивості prop1/prop2 та повертає їх добуток, в противному випадку повертає null
+// Не забудьте, що математичні оператори працюють коректно тільки з числовими значеннями
+
+// const calculator = {
+//   read(a, b) {
+//     this.prop1 = a;
+//     this.prop2 = b;
+//   },
+
+//   sum() {
+//     if( Number(this.prop1)  && Number(this.prop2)) {
+//         return  Number(this.prop1) + Number(this.prop2)
+//     } else {
+//         return null;
+//     }
+
+//   },
+//   mult() {
+//     if( Number(this.prop1)  && Number(this.prop2)) {
+//         return Number(this.prop1) * Number(this.prop2)
+//     } else {
+//         return null;
+//     }
+
+//   },
+// };
+// calculator.read(2, "5");
+
+// console.log(calculator.sum());
+
+// console.log(calculator.mult());
+
+
+
+
+
+
+
